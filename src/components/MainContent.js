30460bb8f18e602efe8a33319e1d76025f835624
davesherlock <dsherlock@sentient.ie>
2021-02-18 10:16:10 +0000

    putting in some content from my portfolio site...

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -2,7 +2,39 @@ import React from "react"
 
 function MainContent() {
     return (
-        <main>This is the content</main>
+        <div id="page-content-wrapper">
+            <div className="container-fluid">
+                <div className="clearfix"><br/>
+                </div>
+                <div className="row" id="icon-div" style="height:50px;">
+                    <div className="col-lg-12"><!--TOGGLE THIS PADDING-->
+                        <span className="pull-right " id="menu-toggle">
+          <i className="fa fa-bars fa-2x" id="menu-icon"></i>
+          <i className="fa fa-times fa-2x" id="close-icon" style="display:none;"></i>
+          </span>
+                    </div>
+                </div>
+                <div className="container">
+                    <div className="row">
+                        <div className="center-block">
+
+                            <div className="left-badger">
+                                <img className="img-circle img-responsive"
+                                     src="https://davesherlock.com/img/me-bw3.jpg"/>
+                            </div>
+                            <div className="right-badger">
+                                <div className="text-wrapper">
+                                    <h1>Dave Sherlock</h1>
+                                    <h4>Web Applications Developer & Interaction Designer</h4>
+                                    <a href="mailto:hello@davesherlock.com"
+                                       target="_blank">hello@davesherlock.com</a><br/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
---
+++ src/components/Sidebar.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+function Sidebar() {
+    return (
+        <div id="sidebar-nav-primary">
+            <ul className="sidebar-nav">
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/portfolio">Portfolio</a></li>
+                <li><a href="https://davesherlock.typeform.com/to/by9Mh4" target="blank">Contact</a></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Sidebar
