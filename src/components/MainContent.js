084043305deb761981f11964335039c16b3d7848
Dave Sherlock <dsherlock@sentient.ie>
2021-03-08 15:20:30 +0000

    fixing more files

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -3,19 +3,19 @@ import React from "react"
 function MainContent() {
     return (
         <div className="row">
-        <div className="center-block">
-          <div className="left-badger">
-            <img className="img-circle img-responsive" alt="me.jpg" src="https://davesherlock.com/img/me-bw3.jpg" />
-          </div>
-          <div className="right-badger">
-            <div className="text-wrapper">
-              <h1>Dave Sherlock</h1>
-              <h4>Web Applications Developer &amp; Interaction Designer</h4>
-              <a href="mailto:hello@davesherlock.com">hello@davesherlock.com</a><br />
+            <div className="center-block">
+                <div className="left-badger">
+                    <img className="img-circle img-responsive" alt="me.jpg" src="https://davesherlock.com/img/me-bw3.jpg" />
+                </div>
+                <div className="right-badger">
+                    <div className="text-wrapper">
+                        <h1>Dave Sherlock</h1>
+                        <h4>Web Applications Developer &amp; Interaction Designer</h4>
+                        <a href="mailto:hello@davesherlock.com">hello@davesherlock.com</a><br />
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
     )
 }
 
